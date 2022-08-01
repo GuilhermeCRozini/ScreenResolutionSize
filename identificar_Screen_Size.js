219e0a144c3233af0656f7af2fdb37c5b91f9886
Guilherme <guicrozini@gmail.com>
2022-08-01 01:24:38 -0300

    Adicionado mais informações na função

--- identificar_Screen_Size.js
+++ identificar_Screen_Size.js
@@ -1,9 +1,10 @@
 const botaoMostra = document.getElementById('botao-mostra');
-const areaMostra = document.getElementById('area-mostra');
+const screenResolution = document.getElementById('display-screen-resolution');
+const devicePixelRatio = document.getElementById('device-pixel-ratio');
+const nativeResolutionMobile = document.getElementById('detect-the-native-resolution-of-a-mobile-device-display');
 
 	function getResolution() {
-		areaMostra.innerHTML = '';
-
-			areaMostra.textContent = `Width: ${screen.width} x Height: ${screen.height}`;
-
+		screenResolution.textContent = `Resolução da Tela: Width: ${screen.width}px x Height: ${screen.height}px`;
+		devicePixelRatio.textContent = `Pixel Ratio do Dispositivo: ${window.devicePixelRatio}`;
+		nativeResolutionMobile.textContent = `Resolução Nativa do Dispositivo: ${window.screen.width * window.devicePixelRatio} x ${window.screen.height * window.devicePixelRatio}`
 	}
